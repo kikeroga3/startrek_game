ed957d60babee0310f53340254ce624add3caa08
kikeroga <kikeroga@gmail.com>
2017-02-14 07:31:49 +0900

    Add files via upload

--- sample/maze.hs
+++ sample/maze.hs
@@ -1,10 +1,12 @@
 title "MAZE"
 
+	sz=32
+
 	randomize
-	color :boxf 0,0,640,480
+	color :boxf 4,4,8*sz+7,8*sz+7
 	color 255,255,255
-	repeat 16 :x=cnt*8+8
-		repeat 16 :y=cnt*8+8
+	repeat sz :x=cnt*8+4+4
+		repeat sz :y=cnt*8+4+4
 			boxf x,y,x+3,y+3
 			a=rnd(4)
 			if a=1 :boxf x,y-4,x+3,y-4+3
